8552f0f9cf047a606ed45e00c29e37bd9625faa3
Kamal Minott <kamalminott@gmail.com>
2022-11-16 11:01:28 +0000

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
+  root "tasks#index"
   # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
 end
